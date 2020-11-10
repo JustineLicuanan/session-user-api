55b11c8939cdb1b27a3a427bcd8bf87856e390dc
Justine Licuanan <caissoone@gmail.com>
2020-11-10 21:05:53 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+// Initializations
+require('dotenv').config();
+const app = express();
+
+// Connect to database
+const { DB_URI, PORT } = process.env;
+mongoose
+	.connect(DB_URI, {
+		// Remove deprecation warnings in the console
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useFindAndModify: false,
+		useCreateIndex: true,
+	})
+	.then(() => {
+		console.log('Connected to database successfully');
+
+		// Start the server
+		app.listen(PORT, (err) => {
+			if (err) throw err;
+			console.log(`Server is listening on port ${PORT}`);
+		});
+	})
+	.catch((err) => console.log(err));
