e3851dbac31a878c3e49078ff55a29e3579fad0c
Justine Licuanan <caissoone@gmail.com>
2020-11-25 14:00:27 +0800

    Create admin's update specific user profile controller

--- controllers/adminControllers.js
+++ controllers/adminControllers.js
@@ -75,7 +75,55 @@ const viewAllUserProfilesGET = async (req, res) => {
 };
 
 // Update specific user profile
-const updateSpecificUserProfilePATCH = (req, res) => {};
+const updateSpecificUserProfilePATCH = async (req, res) => {
+	const { role, name, email, username } = req.body;
+	try {
+		const user = await User.updateOne(
+			{ username: req.params.username },
+			{
+				$set: {
+					role,
+					name,
+					email,
+					username,
+				},
+			},
+			{ runValidators: true }
+		);
+		if (!user.n)
+			return res.status(400).json({
+				err: true,
+				message: 'User does not exist',
+			});
+		res.json({
+			success: true,
+			message: 'User updated successfully',
+		});
+	} catch (error) {
+		let err = {};
+
+		// Handle validation errors
+		if (error._message === 'Validation failed') {
+			Object.keys(error.errors).forEach((errPath) => {
+				err[errPath] = error.errors[errPath].message;
+			});
+			return res.status(400).json({ err });
+		}
+
+		// Handle must-unique props errors
+		if (error.code === 11000 && error.keyPattern.email) {
+			err.email = 'Email is already registered';
+			return res.status(400).json({ err });
+		}
+		if (error.code === 11000 && error.keyPattern.username) {
+			err.username = 'Username is already taken';
+			return res.status(400).json({ err });
+		}
+
+		// Handle other errors
+		res.status(400).json({ err: error });
+	}
+};
 
 // Change specific user password
 const changeSpecificUserPasswordPATCH = (req, res) => {};
